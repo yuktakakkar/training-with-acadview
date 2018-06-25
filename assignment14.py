9ef3b1cffec2c7688c699f622ec66b2b47e7c8b5
yuktakakkar <33526844+yuktakakkar@users.noreply.github.com>
2018-06-25 13:30:10 +0530

    Update assignment14.py

--- assignment14.py
+++ assignment14.py
@@ -44,8 +44,8 @@ with open("test.txt") as f:
 
 Ques-4
 
-with open('test.txt') as fh1, open('test1.txt') as fh2:
-    for line1, line2 in zip(fh1, fh2):
+with open('test.txt') as f1, open('test1.txt') as f2:
+    for line1, line2 in zip(f1, f2):
         # line1 from test.txt, line2 from test2.txt
         print(line1+line2)
 
