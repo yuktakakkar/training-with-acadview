893439b28cc2c5ef43d6c965ba6c8f502acc4e4e
yuktakakkar <33526844+yuktakakkar@users.noreply.github.com>
2018-06-25 22:54:22 +0530

    Update assignment12.py

--- assignment12.py
+++ assignment12.py
@@ -4,7 +4,6 @@ Ques-1
 An access token is an object that describes the security context of a process or thread. The information in a token includes the identity and privileges of the user account associated with the process or thread. When a user logs on, the system verifies the user password by comparing it with information stored in a security database. If the password is authenticated, the system produces an access token. Every process executed on behalf of this user has a copy of this access token.
 """
 create an applicatin on twitter>generate access token
- Access Token 783326174365745153-edVOBYIxgX2IUqUFkEXA28924V02Mjm 
 
 
 Ques-2
